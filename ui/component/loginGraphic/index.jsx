76b90b7977f4d989491c612329cf56cbaf8d51d0
infinite-persistence <inf.persistence@gmail.com>
2021-05-13 17:08:19 +0800

    --PRUNE-BEFORE-MERGE-- point image to github until available in CDN
    Prune this commit before merging.

--- ui/component/loginGraphic/index.jsx
+++ ui/component/loginGraphic/index.jsx
@@ -4,7 +4,9 @@ import { SITE_NAME, LOGIN_IMG_URL } from 'config';
 
 function LoginGraphic(props: any) {
   const [error, setError] = React.useState(false);
-  const src = LOGIN_IMG_URL;
+  const src =
+    LOGIN_IMG_URL ||
+    'https://user-images.githubusercontent.com/67612037/117800473-46150f80-b2a7-11eb-8760-75ee8d35d2b2.png';
 
   return error || !src ? null : (
     <div className="signup-image">
